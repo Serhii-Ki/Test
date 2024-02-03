7eba2f84d9e060bd243a837460c3ca05fc114c17
Sergey Kiba <serhiikiba@gmail.com>
2024-02-03 11:37:23 +0100

    practic sort arr

--- src/components/TestComponent.jsx
+++ src/components/TestComponent.jsx
@@ -1,24 +1,44 @@
+import { useState } from 'react';
+
 const list = [
-	{ id: 1, parent: null },
-	{ id: 2, parent: 1 },
-	{ id: 3, parent: null },
-	{ id: 4, parent: 2 },
-	{ id: 5, parent: 3 },
-	{ id: 6, parent: 5 },
-	{ id: 7, parent: 6 },
-	{ id: 8, parent: 2 },
-	{ id: 9, parent: 3 },
-	{ id: 10, parent: 5 },
+	{ id: 1, banknotes: 'dollars', value: 100 },
+	{ id: 2, banknotes: 'dollars', value: 130 },
+	{ id: 3, banknotes: 'zloty', value: 1100 },
+	{ id: 4, banknotes: 'zloty', value: 321 },
+	{ id: 5, banknotes: 'dollars', value: 453 },
+	{ id: 6, banknotes: 'zloty', value: 1531 },
+	{ id: 7, banknotes: 'zloty', value: 422 },
 ];
 function TestComponent() {
+	const [banknotes, setBanknotes] = useState('all');
+
+	const onChangeFilter = filter => {
+		setBanknotes(filter);
+	};
+
+	const renderElem = () => {
+		if (banknotes === 'dollars') {
+			return list.filter(item => item.banknotes === 'dollars');
+		}
+		if (banknotes === 'zloty') {
+			return list.filter(item => item.banknotes === 'zloty');
+		}
+		return list;
+	};
+
 	return (
-		<ul>
-			{list
-				.filter(item => item.id === item.parent)
-				.map(item => {
-					return <li>{item}</li>;
-				})}
-		</ul>
+		<div>
+			<button onClick={() => onChangeFilter('all')}>All</button>
+			<button onClick={() => onChangeFilter('dollars')}>dollars</button>
+			<button onClick={() => onChangeFilter('zloty')}>zloty</button>
+			<ul>
+				{renderElem().map(item => (
+					<li>
+						{item.value} {item.banknotes}
+					</li>
+				))}
+			</ul>
+		</div>
 	);
 }
 
