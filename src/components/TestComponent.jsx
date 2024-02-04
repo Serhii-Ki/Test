ac24c8b967d51621a0bf2886a460534608ff7ebb
Sergey Kiba <serhiikiba@gmail.com>
2024-02-04 22:29:35 +0100

    solving react tasks

--- src/components/TestComponent.jsx
+++ src/components/TestComponent.jsx
@@ -1,43 +1,26 @@
-import { useState } from 'react';
-
 const list = [
-	{ id: 1, banknotes: 'dollars', value: 100 },
-	{ id: 2, banknotes: 'dollars', value: 130 },
-	{ id: 3, banknotes: 'zloty', value: 1100 },
-	{ id: 4, banknotes: 'zloty', value: 321 },
-	{ id: 5, banknotes: 'dollars', value: 453 },
-	{ id: 6, banknotes: 'zloty', value: 1531 },
-	{ id: 7, banknotes: 'zloty', value: 422 },
+	{ value: 'ser3', order: 3, expired: false },
+	{ value: 'ser4', order: 4, expired: false },
+	{ value: 'ser1', order: 1, expired: true },
+	{ value: 'ser2', order: 2, expired: false },
 ];
 function TestComponent() {
-	const [banknotes, setBanknotes] = useState('all');
+	const res = list
+		.filter(item => !item.expired)
+		.sort((a, b) => a.order - b.order)
+		.map(item => {
+			return Array.from(item.value).reverse();
+		})
+		.flat();
 
-	const onChangeFilter = filter => {
-		setBanknotes(filter);
-	};
+	const res1 = Array.from(new Set(res)).join(' ');
 
-	const renderElem = () => {
-		if (banknotes === 'dollars') {
-			return list.filter(item => item.banknotes === 'dollars');
-		}
-		if (banknotes === 'zloty') {
-			return list.filter(item => item.banknotes === 'zloty');
-		}
-		return list;
-	};
-
+	console.log(res1);
 	return (
 		<div>
-			<button onClick={() => onChangeFilter('all')}>All</button>
-			<button onClick={() => onChangeFilter('dollars')}>dollars</button>
-			<button onClick={() => onChangeFilter('zloty')}>zloty</button>
-			<ul>
-				{renderElem().map(item => (
-					<li>
-						{item.value} {item.banknotes}
-					</li>
-				))}
-			</ul>
+			{res.map(item => (
+				<div>{item}</div>
+			))}
 		</div>
 	);
 }
