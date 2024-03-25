5a301a82ac52b18c37dbaa89bc84640e36481a01
Sergey Kiba <serhiikiba@gmail.com>
2024-03-25 18:53:56 +0100

    traind render component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,12 @@ import './app.css';
 
 import AppTest from "./components/testComponents/AppTest";
 import Timer from "./components/testComponents/Timer";
+import {RenderImproveApp} from "./components/renderImprove/RenderImprove";
 
 function App() {
 	return (
 		<div className='app'>
-			<AppTest/>
-			<Timer/>
+			<RenderImproveApp/>
 		</div>
 	);
 }
