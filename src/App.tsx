05b792b7e7d178a94cb8e1ace216f692dd8a3349
Kiba Sergey <serhiikiba@gmail.com>
2024-04-21 18:35:05 +0200

    fixed merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-import './App.css'
 import CounterApp from "./counterSrc/CounterApp.tsx";
 
 function App() {
@@ -13,20 +11,4 @@ function App() {
 }
 
 export default App
-=======
-import './app.css';
 
-import AppTest from "./components/testComponents/AppTest";
-import Timer from "./components/testComponents/Timer";
-import {RenderImproveApp} from "./components/renderImprove/RenderImprove";
-
-function App() {
-	return (
-		<div className='app'>
-			<RenderImproveApp/>
-		</div>
-	);
-}
-
-export default App;
->>>>>>> 5a301a82ac52b18c37dbaa89bc84640e36481a01
