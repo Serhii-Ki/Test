8855f00a43a928b32e137ed07031ba30b7f9c8e0
Kiba Sergey <serhiikiba@gmail.com>
2024-10-09 22:41:43 +0200

    custom reduce

--- src/test/AppTest.tsx
+++ src/test/AppTest.tsx
@@ -1,24 +1,12 @@
-function myPromiseAll(promises: Promise[]) {
-  return new Promise((resolve, reject) => {
-    if (!promises.length) {
-      resolve([]);
-    }
-    const result = [];
-    let promiseCount = 0;
+Array.prototype.myReduce = function (callback, initialValue) {
+  let acc = initialValue || this[0];
+  let startIndex = initialValue ? 0 : 1;
 
-    promises.forEach((promise, index) => {
-      promise
-        .then((res) => {
-          result[index] = res;
-          promiseCount++;
+  for (let i = startIndex; i < this.length; i++) {
+    acc = callback(acc, this[i], i, this);
+  }
 
-          if (promiseCount === result.length) {
-            resolve(result);
-          }
-        })
-        .catch((err) => {
-          reject(err);
-        });
-    });
-  });
-}
+  return acc;
+};
+const arr = [1, 2, 3, 4];
+console.log(arr.myReduce((acc, item) => acc + item));
